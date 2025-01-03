9516a2adcd9faa73cfd4c836f7b0e9aee99df7f0
LeeMineo <lms4882@gmail.com>
2025-01-03 10:56:38 +0900

    /

--- rfd_intro/src/App.js
+++ rfd_intro/src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import Header from './components/Header';
+//import Header from './components/Header';
 import Intro from './sections/Intro';
+import AboutUs from './sections/AboutUs';
+import HowItWorks from './sections/HowItWorks';
+import Features from './sections/Features';
+import CommunityHighlights from './sections/CommunityHighlights';
+import PlansPricing from './sections/PlansPricing';
+import CTA from './sections/CTA';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
-      <Header />
+      {/* <Header /> */}
       <Intro />
       {/* 다른 섹션 추가 */}
+      <AboutUs />
+      <HowItWorks />
+      <Features />
+      <CommunityHighlights />
+      <PlansPricing />
+      <CTA />
+      <Footer />
     </div>
   );
 }
